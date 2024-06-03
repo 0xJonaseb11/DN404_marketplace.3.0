8ee91368fe8c4f40dcab382fd1cd14b11a41e7e4
Jaz-3-0 <sebejaz99@gmail.com>
2024-06-03 20:47:02 +0200

    init update deploy marketplace and token contracts

---
+++ smart_contract/scripts/deployMarketplace.js
@@ -0,0 +1,23 @@
+const { ethers } = require("hardhat");
+
+const main = async() => {
+
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying contract with account:", deployer.address);
+
+    const NFTMarketplace = await ethers.getContractFactory("NFTMarketplace");
+    const nftMarketplace = await NFTMarketplace.deploy();
+
+    console.log("NFTMarketplace contract deployed to:", await nftMarketplace.getAddress());
+}
+
+const runMain = async() => {
+    try {
+        await main();
+        process.exit(0);
+    } catch(err) {
+        console.error("Errr deploying NFTMarketplace contract",err);
+        process.exit(1);
+    }
+}
---
+++ smart_contract/scripts/deployToken.js
@@ -0,0 +1,50 @@
+const { ethers } = require("hardhat");
+const fs = require("fs");
+
+const main = async() => {
+  const [deployer] = await ethers.getSigners();
+  const name = "Meta";
+  const symbol = "Meta";
+  const maxSupply = ethers.parseEther("50");
+  const publicPrice = ethers.parseEther("0");
+  const initialTokenSupply = ethers.parseEther("0");
+  const signer = deployer.address;
+
+  const argumentsArray = [
+    name, 
+    symbol, 
+    maxSupply.toString(), 
+    publicPrice.toString(), 
+    initialTokenSupply.toString(), 
+    signer
+  ];
+
+  const content = "module.exports = " + JSON.stringify(argumentsArray, null, 2) +";";
+  fs.writeFileSync("./arguments.js", content);
+
+  console.log("Arguments.js file generated successfully");
+
+  console.log("Deploying contract with account:", deployer.address);
+
+  const Token = await ethers.getContractFactory("NFTMintDN404";
+    const token = await Token.deploy(
+      name,
+      symbol,
+      maxSupply,
+      publicPrice,
+      initialTokenSupply,
+      signer
+    );
+
+    console.log("Fractionalized NFT deployed to:", await token.getAddress());
+}
+
+const runMain = async() => {
+  try {
+    await main();
+    process.exit(0);
+  } catch(err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
